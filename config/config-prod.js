9875849e3c7647438f2b7d6b79ff2980f6de3caf
chenheng <chenheng@chenhengdeMacBook-Pro.local>
2020-05-20 13:47:16 +0800

    1

--- config/config-prod.js
+++ config/config-prod.js
@@ -5,7 +5,7 @@ const config = {
     },
     redis: {
         port: 6379,
-        host: '49.234.113.173',
+        host: '127.0.0.1',
         prefix: 'sam:', //存诸前缀
         // ttl: 60 * 60 * 23,  //过期时间
         family: 4, // 4 (IPv4) or 6 (IPv6)
